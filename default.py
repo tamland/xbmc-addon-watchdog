413c97e9d02447f163ceb0c893f6e2ea38b7aa05
Thomas Amland <thomas.amland@gmail.com>
2012-04-14 19:39:51 +0200

    rewrite. new backend

--- default.py
+++ default.py
@@ -1,167 +1,125 @@
-import os, sys, time, math
-import xbmc, xbmcaddon
-from pyinotify import *
+'''
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import os
+import simplejson
+import xbmc
+import xbmcaddon
+import debugging
+from time import sleep
 from threading import Thread
-from autoexecHandler import AutoexecHandler
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-ADDON_ID      = "script.autoupdater"
-FILENAME_THIS = "default.py"
 
-SETTINGS      = xbmcaddon.Addon(id=ADDON_ID)
-DELAY         = math.fabs(int(SETTINGS.getSetting("delay"))) #delay before executing the update function
-AUTOEXEC      = SETTINGS.getSetting("autoexec") in ["true", "True", "1"]
-CLEAN_VIDEO   = SETTINGS.getSetting("vclean") in ["true", "True", "1"]
-CLEAN_MUSIC   = SETTINGS.getSetting("mclean") in ["true", "True", "1"]
+ADDON    = xbmcaddon.Addon()
+ADDON_ID = ADDON.getAddonInfo('id')
+CLEAN    = ADDON.getSetting("clean") in ["true", "True", "1"]
+DELAY    = 1
 
-PATHS_VIDEO = []
-PATHS_MUSIC = []
-for i in range(0, 4):
-	pv = SETTINGS.getSetting("vpath" + str(i))
-	pm = SETTINGS.getSetting("mpath" + str(i))
-	if not("" == pv.strip()): PATHS_VIDEO.append(xbmc.translatePath(pv))
-	if not("" == pm.strip()): PATHS_MUSIC.append(xbmc.translatePath(pm))
 
-SETTINGS = None
-MASK = EventsCodes.IN_CREATE | EventsCodes.IN_DELETE | EventsCodes.IN_MOVED_TO | EventsCodes.IN_MOVED_FROM |EventsCodes.IN_MOVE_SELF
-
-	
 class Worker(Thread):
-	def __init__(self):
-		Thread.__init__(self)
-		self.q1 = []
-		self.q2 = []
-		
-	def queue1(self, f, b):
-		self.q1.append((f,b))
-		
-	def queue2(self, f, b):
-		self.q2.append((f,b))
-		
-	def run(self):
-		time.sleep(DELAY)
-		
-		while (len(self.q1) > 0) or (len(self.q2) > 0):
-			if len(self.q1) > 0:
-				while len(self.q1) > 0:
-					(f, b) = self.q1.pop()
-				f()
-				time.sleep(0.2) #Without this getCondVisibility(window) wont work. It returns false until gui is actually rendered
-				while b():
-					time.sleep(1)
+  def __init__(self):
+    Thread.__init__(self)
+    self.scan = False
+    self.clean = False
+  
+  def run(self):
+    sleep(DELAY)
+    while True:
+      if self.clean:
+        self.clean = False
+        xbmc.log("%s cleaning library" % (ADDON_ID), xbmc.LOGDEBUG)
+        xbmc.executebuiltin("CleanLibrary(video)")
+        sleep(1) #give xbmc time to render
+        while xbmc.getCondVisibility('Window.IsActive(10101)'):
+          sleep(1)
+      
+      if self.scan:
+        xbmc.log("%s scanning library" % (ADDON_ID), xbmc.LOGDEBUG)
+        self.scan = False
+        xbmc.executebuiltin("UpdateLibrary(video)")
+        sleep(1)
+        while xbmc.getCondVisibility('Library.IsScanning'):
+          sleep(1)
+      
+      #done if no new requests since scan/clean was set to false
+      if not(self.scan) and not(self.clean):
+        return
 
-			if len(self.q2) > 0:
-				while len(self.q2) > 0:
-					(f, b) = self.q2.pop()
-				f()
-				time.sleep(0.2)
-				while b():
-					time.sleep(1)
 
-class Xbmc:
-	def __init__(self, library, clean):
-		self.worker = Worker()
-		self.worker.start()
-		self.library = library
-		self.doClean = clean
-		
-	def __scanf(self):
-		xbmc.executebuiltin("UpdateLibrary(" + self.library + ")")
-	
-	def __cleanf(self):
-		xbmc.executebuiltin("CleanLibrary(" + self.library + ")")
-	
-	def __isScanningf(self):
-		return xbmc.getCondVisibility('Library.IsScanning')
-	
-	def __isCleaningf(self):
-		return xbmc.getCondVisibility('Window.IsActive(10101)') #progressdialog
-	
-	def scan(self):
-		if(self.worker.isAlive()):
-			self.worker.queue1( self.__scanf, self.__isScanningf )
-		else:
-			self.worker = Worker()
-			self.worker.queue1( self.__scanf, self.__isScanningf )
-			self.worker.start()
-			
-	def clean(self):
-		if self.doClean:
-			if(self.worker.isAlive()):
-				self.worker.queue2( self.__cleanf, self.__isCleaningf )
-			else:
-				self.worker = Worker()
-				self.worker.queue2( self.__cleanf, self.__isCleaningf )
-				self.worker.start()
+class EventHandler(FileSystemEventHandler):
+  def __init__(self):
+    FileSystemEventHandler.__init__(self)
+    self.worker = Worker()
+  
+  def _scan(self):
+    self._on_worker('scan')
+  
+  def _clean(self):
+    if CLEAN: self._on_worker('clean')
+  
+  def _on_worker(self, attr):
+    if self.worker.isAlive():
+      setattr(self.worker, attr, True)
+    else:
+      self.worker = Worker()
+      setattr(self.worker, attr, True)
+      self.worker.start()
+  
+  def on_created(self, event):
+    self._scan()
+  
+  def on_deleted(self, event):
+    self._clean()
+  
+  def on_moved(self, event):
+    self._clean()
+    self._scan()
+  
+  def on_any_event(self, event):
+    xbmc.log("%s <%s> <%s>" % (ADDON_ID, str(event.event_type), str(event.src_path)), xbmc.LOGDEBUG)
 
-class ProcHandler(ProcessEvent):
-	def __init__(self, wm, xbmch):
-		self.wm = wm
-		self.xbmch = xbmch
-		
-	def process_IN_CREATE(self, event):
-		log(event)
-		path = event.path + "/" + event.name
-		if os.path.isdir(path) and len(os.listdir(path)) == 0:
-			return
-		self.xbmch.scan()
 
-	def process_IN_DELETE(self, event):
-		log(event)
-		self.xbmch.clean()
+def get_media_sources():
+  query = '{"jsonrpc": "2.0", "method": "Files.GetSources", "params": {"media": "video"}, "id": 1}'
+  result = xbmc.executeJSONRPC(query)
+  json = simplejson.loads(result)
+  if json.has_key('result'):
+    if json['result'].has_key('sources'):
+      return [ e['file'] for e in json['result']['sources'] ]
+  return []
 
-	def process_IN_MOVE_SELF(self, event):
-		log(event)
-		if event.path.endswith("invalided-path"): #moved outside
-			self.wm.rm_watch(event.wd, rec=True)
-			
-	def process_IN_IGNORED(self, event):
-		log(event)
 
-	def process_IN_MOVED_FROM(self, event):
-		log(event)
-		self.xbmch.clean()
-		
-	def process_IN_MOVED_TO(self, event):
-		log(event)
-		path = event.path + "/" + event.name
-		if os.path.isdir(path) and len(os.listdir(path)) == 0:
-			return 
-		self.xbmch.scan()
-		
-def log(event):
-	v = (event.event_name, event.is_dir, event.mask, event.name, event.path)
-	print ADDON_ID + ": event_name: %s\t   is_dir: %s\t   mask: %s\t   name: %s\t   path: %s" % v
+if __name__ == "__main__":
+  event_handler = EventHandler()
+  observer = Observer()
+  
+  xbmc.log("%s: using <%s>" % (ADDON_ID, debugging.get_observer_name()), xbmc.LOGDEBUG)
+  
+  dirs = get_media_sources()
+  for item in dirs:
+    if os.path.exists(item):
+      xbmc.log("%s: watching <%s>" % (ADDON_ID, item), xbmc.LOGDEBUG)
+      observer.schedule(event_handler, path=item, recursive=True)
+    else:
+      xbmc.log("%s: not watching <%s>" % (ADDON_ID, item), xbmc.LOGDEBUG)
+  
+  observer.start()
+  while (not xbmc.abortRequested):
+    sleep(1)
+  observer.stop()
+  observer.join()
 
-def newInstance(library, paths, clean):
-	xbmch = Xbmc(library, clean)
-	wm = WatchManager()
-	phandler = ProcHandler(wm, xbmch)
-	notifier = ThreadedNotifier(wm, phandler)
-	notifier.start()
-	for p in paths:
-		wm.add_watch(p, MASK, rec=True, auto_add=True)
-
-def main():
-	aeh = AutoexecHandler()
-	if AUTOEXEC:
-		aeh.add(ADDON_ID, FILENAME_THIS)
-	else:
-		aeh.remove(ADDON_ID, FILENAME_THIS)
-	aeh = None
-	
-	
-	if len(PATHS_VIDEO) == 0 and len(PATHS_MUSIC) == 0: #nothing to watch
-		return
-		
-	if len(PATHS_VIDEO) > 0:
-		newInstance("video", PATHS_VIDEO, CLEAN_VIDEO)
-	if len(PATHS_MUSIC) > 0:
-		newInstance("music", PATHS_MUSIC, CLEAN_MUSIC)
-	
-	
-	#keep self alive or else python will hang when exiting xbmc
-	while True:
-		time.sleep(1)
-		
-if ( __name__ == "__main__" ):
-	main()
