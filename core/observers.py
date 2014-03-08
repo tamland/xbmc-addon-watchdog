e7191acb98b6f2966a00507dc4b36407d8c17c42
Thomas Amland <thomas.amland@gmail.com>
2014-03-08 17:31:45 +0100

    dont try to import FSEvents/WinAsync
    will always fail anyway (fsevent require c extension,
    async not implemented yet)

--- core/observers.py
+++ core/observers.py
@@ -23,16 +23,10 @@ try:
     from watchdog.observers.inotify import InotifyObserver as _Observer
 except ImportError:
     try:
-        from watchdog.observers.fsevents import FSEventsObserver as _Observer
+        from watchdog.observers.kqueue import KqueueObserver as _Observer
     except ImportError:
         try:
-            from watchdog.observers.kqueue import KqueueObserver as _Observer
-        except ImportError:
-            try:
-                from watchdog.observers.read_directory_changes_async import WindowsApiAsyncObserver as _Observer
-            except ImportError:
-                try:
-                    from watchdog.observers.read_directory_changes import WindowsApiObserver as _Observer
-                except (ImportError, AttributeError):
-                    _Observer = local_full
+            from watchdog.observers.read_directory_changes import WindowsApiObserver as _Observer
+        except (ImportError, AttributeError):
+            _Observer = local_full
 auto = _Observer
