84ed321cb320219b73b0f93092f2669c880a9fb3
Thomas Amland <thomas.amland@gmail.com>
2014-04-01 14:12:44 +0200

    remove unused imports

--- core/observers.py
+++ core/observers.py
@@ -12,8 +12,6 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
-from polling_local import PollerObserver_Depth1 as local_depth_1
-from polling_local import PollerObserver_Depth2 as local_depth_2
 from polling_local import PollerObserver_Full as poller_local
 from polling_xbmc import PollerObserver_Depth1 as xbmc_depth_1
 from polling_xbmc import PollerObserver_Depth2 as xbmc_depth_2
